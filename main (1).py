81b52d731a9be0e4b3f901b755a30cd02d9d94ef
Besthapavithra <besthapavithra238@gmail.com>
2025-12-24 10:12:46 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+
+x = int(input("Enter a number: "))
+for i in range(1, x + 1):
+    if (i % 3 == 0) and (i % 5 == 0):
+        
+        print("fizzbuzz") 
+    elif (i % 3 == 0):
+        
+        print("fizz") 
+    elif (i % 5 == 0):
+        
+        print("buzz") 
+    else:
+      
+        print(i) 
