d649f9fb738da9750b875fd68f0247f9c249df17
Besthapavithra <besthapavithra238@gmail.com>
2025-12-26 14:48:10 +0530

    Add files via upload

---
+++ leftover letter.py
@@ -0,0 +1,21 @@
+def leftover(a,b):
+    left=" "
+    remove=set(b)
+    
+    for ch in a:
+        
+        if ch not in remove:
+            
+            
+            left+=ch
+            
+    if left:
+                
+        print(left)
+               
+    else:
+        print("empty")
+            
+a="ABCDEF"
+b="BDEF"
+leftover(a,b)
